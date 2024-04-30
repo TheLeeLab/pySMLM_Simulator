786611a4ec8fc903fba6e3bb38cf50f462dd45f3
jbeckwith <jsbeckwith@gmail.com>
2024-04-30 12:13:51 +0100

    Updated Requirements.txt

--- src/PSFFunctions.py
+++ src/PSFFunctions.py
@@ -25,11 +25,11 @@ class PSF_Functions():
         calculates diffraction limit from Abbe criterion
         
         Args:
-        - wavelength (float). wavelength of light being imaged
-        - NA numerical aperture of microscope
+            wavelength (float): wavelength of light being imaged
+            NA (float): numerical aperture of microscope
     
         Returns:
-        - diffraction_limit (float). d of psf
+            diffraction_limit (float): d of psf
         """
         return np.divide(wavelength, np.multiply(2., NA))
 
@@ -39,16 +39,16 @@ class PSF_Functions():
         x and y, with the origin x0 and y0
         
         Args:
-        - x (1d array). x coordinate locations, in same unit as sigma_psf (typically micron)
-        - x (1d array). y coordinate locations, in same unit as sigma_psf (typically micron)
-        - sigma_psf (float). width of 2d gaussian. 
-        - x0 (float or 1d array). origin position of 2d gaussian in x, in same unit as sigma_psf
-        - y0 (float or 1d array). origin position of 2d gaussian in y, in same unit as sigma_psf
-        - n_photons (int). n_photons per localisation. Given to poisson rng
-        - bitdepth (type). bit depth. default uint16        
+            x (1d array): x coordinate locations, in same unit as sigma_psf (typically micron)
+            x (1d array): y coordinate locations, in same unit as sigma_psf (typically micron)
+            sigma_psf (float): width of 2d gaussian. 
+            x0 (float or 1d array): origin position of 2d gaussian in x, in same unit as sigma_psf
+            y0 (float or 1d array): origin position of 2d gaussian in y, in same unit as sigma_psf
+            n_photons (int): n_photons per localisation. Given to poisson rng
+            bitdepth (type): bit depth. default uint16        
         
         Returns:
-        - PSF_g2d (2D array). 2d probability density function of PSF
+            PSF_g2d (2D array): 2d probability density function of PSF
         """
         X, Y = np.meshgrid(x, y)
         nfactor = np.multiply(sigma_psf, np.sqrt(2.*np.pi))
@@ -69,15 +69,15 @@ class PSF_Functions():
         x and y, with the origin x0 and y0
         
         Args:
-        - image_size (tuple). tuple of how big the image is in pixels
-        - sigma_psf (float). width of 2d gaussian in pixels 
-        - x0 (float or 1d array). origin position of 2d gaussian in x, in pixels
-        - y0 (float or 1d array). origin position of 2d gaussian in y, in pixels
-        - n_photons (int). n_photons per localisation. Given to poisson rng
-        - bitdepth (type). bit depth. default uint16        
+            image_size (tuple): tuple of how big the image is in pixels
+            sigma_psf (float): width of 2d gaussian in pixels 
+            x0 (float or 1d array): origin position of 2d gaussian in x, in pixels
+            y0 (float or 1d array): origin position of 2d gaussian in y, in pixels
+            n_photons (int): n_photons per localisation. Given to poisson rng
+            bitdepth (type): bit depth. default uint16        
         
         Returns:
-        - PSF_g2d (2D array). 2d probability density function of PSF
+            PSF_g2d (2D array): 2d probability density function of PSF
         """
         PSF_g2d = np.zeros(image_size, dtype=bitdepth)
 
@@ -96,15 +96,15 @@ class PSF_Functions():
         Biophys J., 2004
         
         Args:
-        - image_size (tuple). tuple of how big the image is in pixels
-        - lambda_sensor (float). mean of possion random variable for background noise
-        - mu_sensor (float). mean of gaussian for camera read noise
-        - sigma_sensor (float). sigma of gaussian for camera read noise
-        - bitdepth (type). bit depth. default uint16
+            image_size (tuple): tuple of how big the image is in pixels
+            lambda_sensor (float): mean of possion random variable for background noise
+            mu_sensor (float): mean of gaussian for camera read noise
+            sigma_sensor (float): sigma of gaussian for camera read noise
+            bitdepth (type): bit depth. default uint16
     
         Returns:
-        - image_matrix (ND array). ND image matrix with noise added to simulate
-        detector noise
+            image_matrix (numpy.ndarray): ND image matrix with noise added to simulate
+                                    detector noise
         """
         image_matrix = np.add(np.asarray(np.random.poisson(lambda_sensor, size=(image_size)), dtype=bitdepth),
                     np.asarray(np.random.normal(loc=mu_sensor, scale=sigma_sensor, size=(image_size)), dtype=bitdepth))
@@ -118,25 +118,25 @@ class PSF_Functions():
         pixels in an image
         
         Args:
-        - image_size (tuple). tuple of how big the image is in pixels
-        - labelled_pixels (1d array). pixel indices of where labels are
-        - n_photons (int). number of photons per localisation
-        - n_frames (int). number of frames to make up the super-res trace
-        - labelling_density (float). how many of the pixels will be labelled
-            across the whole imaging simulation
-        - pixel_size (float). Default 0.11 micron, how large pixel sizes are
-        - imaging_wavelength (float). Default is 0.52 microns. Imaging wavelength
-        - NA (float). Default 1.49 micron, defines how large your PSF will be
-        - lambda_sensor (float). mean of poisson rnv
-        - mu_semsor (float) mean of gaussian for camera read noise
-        - sigma_sensor (float) sigma of gaussian read noise
-        - bitdepth (type). bit depth. default float32
+            image_size (tuple): tuple of how big the image is in pixels
+            labelled_pixels (1d array): pixel indices of where labels are
+            n_photons (int): number of photons per localisation
+            n_frames (int): number of frames to make up the super-res trace
+            labelling_density (float): how many of the pixels will be labelled
+                                    across the whole imaging simulation
+            pixel_size (float): Default 0.11 micron, how large pixel sizes are
+            imaging_wavelength (float): Default is 0.52 microns. Imaging wavelength
+            NA (float): Default 1.49, defines how large your PSF will be
+            lambda_sensor (float): mean of poisson rnv
+            mu_semsor (float): mean of gaussian for camera read noise
+            sigma_sensor (float): sigma of gaussian read noise
+            bitdepth (type): bit depth. default float32
 
         Returns:
-        - superres_image_matrix (ND array). ND image matrix with noise and PSFs added.
-        - superres_cumsum_matrix (ND array). ND image matrix of cumulative localisations.
-        - dl_image_matrix (2D array). Equivalent diffraction-limited image.
-        - supreres_image (2D array). Final superres image.
+            superres_image_matrix (numpy.ndarray). ND image matrix with noise and PSFs added.
+            superres_cumsum_matrix (numpy.ndarray). ND image matrix of cumulative localisations.
+            dl_image_matrix (numpy.2darray). Equivalent diffraction-limited image.
+            supreres_image (numpy.2darray). Final superres image.
         """
         stack_size = (image_size[0], image_size[1], n_frames)
         sigma_psf = self.diffraction_limit(imaging_wavelength, NA)
@@ -178,25 +178,25 @@ class PSF_Functions():
         pixels in an image
         
         Args:
-        - image_size (tuple). tuple of how big the image is in pixels
-        - labelled_pixels (1d array). pixel indices of where labels are
-        - n_photons (int). number of photons per localisation
-        - n_frames (int). number of frames to make up the super-res trace
-        - labelling_density (float). how many of the pixels will be labelled
-            across the whole imaging simulation
-        - pixel_size (float). Default 0.11 micron, how large pixel sizes are
-        - imaging_wavelength (float). Default is 0.52 microns. Imaging wavelength
-        - NA (float). Default 1.49 micron, defines how large your PSF will be
-        - lambda_sensor (float). mean of poisson rnv
-        - mu_semsor (float) mean of gaussian for camera read noise
-        - sigma_sensor (float) sigma of gaussian read noise
-        - bitdepth (type). bit depth. default float32
+            image_size (tuple): tuple of how big the image is in pixels
+            labelled_pixels (1d array): pixel indices of where labels are
+            n_photons (int): number of photons per localisation
+            n_frames (int): number of frames to make up the super-res trace
+            labelling_density (float): how many of the pixels will be labelled
+                                        across the whole imaging simulation
+            pixel_size (float): Default 0.11 micron, how large pixel sizes are
+            imaging_wavelength (float): Default is 0.52 microns. Imaging wavelength
+            NA (float): Default 1.49, defines how large your PSF will be
+            lambda_sensor (float): mean of poisson rnv
+            mu_semsor (float): mean of gaussian for camera read noise
+            sigma_sensor (float): sigma of gaussian read noise
+            bitdepth (type): bit depth. default float32
 
         Returns:
-        - superres_image_matrix (ND array). ND image matrix with noise and PSFs added.
-        - superres_cumsum_matrix (ND array). ND image matrix of cumulative localisations.
-        - dl_image_matrix (2D array). Equivalent diffraction-limited image.
-        - supreres_image (2D array). Final superres image.
+            superres_image_matrix (numpy.ndarray): ND image matrix with noise and PSFs added.
+            superres_cumsum_matrix (numpy.ndarray): ND image matrix of cumulative localisations.
+            dl_image_matrix (numpy.2darray): Equivalent diffraction-limited image.
+            supreres_image (numpy.2darray): Final superres image.
         """
         stack_size = (image_size[0], image_size[1], n_frames)
         sigma_psf = self.diffraction_limit(imaging_wavelength, NA)
